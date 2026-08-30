8b4ae62fbe62d2e739d03428f49714baf14a69f1
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-29 23:39:35 -0500

    exercise: rescale_video.py

---
+++ exercises/rescales/rescale_video.py
@@ -0,0 +1,28 @@
+import os
+import cv2 as cv
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+capture_path = os.path.join(os.path.dirname(__file__), '../..', 'Videos', 'dog.mp4')
+
+capture = cv.VideoCapture(capture_path)
+
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame)
+
+    cv.imshow('Video', frame_resized)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+
+cv.destroyAllWindows()
+
