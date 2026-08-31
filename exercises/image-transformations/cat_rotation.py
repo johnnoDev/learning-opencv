8390060b7e194f66a54ecc44df838e483e8cf886
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-31 15:41:59 -0500

    2-rotations and more exercises

--- exercises/image-transformations/cat_rotation.py
+++ exercises/image-transformations/cat_rotation.py
@@ -2,7 +2,7 @@ import os
 import cv2 as cv
 import numpy as np
 
-img_path = os.path.join(os.path.dirname(__file__), '..', 'Photos', 'park.jpg')
+img_path = os.path.join(os.path.dirname(__file__), '../..', 'Photos', 'cats.jpg')
 img = cv.imread(img_path)
 cv.imshow('Original', img)
 
@@ -11,13 +11,12 @@ def rotate(img, angle, rotPoint=None):
     if rotPoint is None:
         rotPoint = (width//2, height//2)
 
-    rotMat = cv.getRotationMatrix2D(rotPoint, angle, scale=1.0)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, scale=0.5)
     dimensions = (width, height)
 
     return cv.warpAffine(img, rotMat, dimensions)
 
 rotated = rotate(img, -45)
-
 cv.imshow('Image rotated', rotated)
 
 cv.waitKey(0)
